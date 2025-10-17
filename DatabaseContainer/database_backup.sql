7d7775cd5f5c323a7561089e68f373424b2f132c
Kavia AI <production@kavia.ai>
2025-10-17 06:08:06 +0000

    Auto-commit: session cgc503609a - notes-app-28436-28694

--- DatabaseContainer/database_backup.sql
+++ DatabaseContainer/database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict MXgQkZ3e36OBfO8ua54KMbf22bqPtpzF4IGwFvj6I9py2pnTe5pPctZpBQCa4u4
+\restrict pHeLjDA3V0HHfAP7qQaRARRIqEYKVpg2hnhqAB1uE4cynkKbOeK42a4jaqwpT3r
 
 -- Dumped from database version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
 -- Dumped by pg_dump version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
@@ -28,9 +28,9 @@ CREATE DATABASE myapp WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDE
 
 ALTER DATABASE myapp OWNER TO postgres;
 
-\unrestrict MXgQkZ3e36OBfO8ua54KMbf22bqPtpzF4IGwFvj6I9py2pnTe5pPctZpBQCa4u4
+\unrestrict pHeLjDA3V0HHfAP7qQaRARRIqEYKVpg2hnhqAB1uE4cynkKbOeK42a4jaqwpT3r
 \connect myapp
-\restrict MXgQkZ3e36OBfO8ua54KMbf22bqPtpzF4IGwFvj6I9py2pnTe5pPctZpBQCa4u4
+\restrict pHeLjDA3V0HHfAP7qQaRARRIqEYKVpg2hnhqAB1uE4cynkKbOeK42a4jaqwpT3r
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -89,5 +89,5 @@ ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES 
 -- PostgreSQL database dump complete
 --
 
-\unrestrict MXgQkZ3e36OBfO8ua54KMbf22bqPtpzF4IGwFvj6I9py2pnTe5pPctZpBQCa4u4
+\unrestrict pHeLjDA3V0HHfAP7qQaRARRIqEYKVpg2hnhqAB1uE4cynkKbOeK42a4jaqwpT3r
 
